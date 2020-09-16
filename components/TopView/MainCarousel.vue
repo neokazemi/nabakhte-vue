f56070d611c73f211b5b3902f3e906870c5d65f1
kerasus <aliesmaeeli20@gmail.com>
2020-09-16 15:54:06 +0430

    home page get products

--- components/TopView/MainCarousel.vue
+++ components/TopView/MainCarousel.vue
@@ -4,10 +4,10 @@
     :hide-delimiters="true"
   >
     <v-carousel-item>
-      <v-img src="https://media.chibekhoonam.net/2020/08/اسلاید-شو.jpg" />
+      <v-img src="https://media.chibekhoonam.net/2020/09/nazarsanji-hafte3-shahrivar.jpg" />
     </v-carousel-item>
     <v-carousel-item>
-      <v-img src="https://media.chibekhoonam.net/2020/09/nazarsanji-hafte3-shahrivar.jpg" />
+      <v-img src="https://media.chibekhoonam.net/2020/08/اسلاید-شو.jpg" />
     </v-carousel-item>
   </v-carousel>
 </template>
