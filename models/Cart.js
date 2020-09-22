a81ef7d8be331393bc38a63ea7b2f7f9bfbde901
kerasus <aliesmaeeli20@gmail.com>
2020-09-22 14:47:15 +0330

    Cart model

--- models/Cart.js
+++ models/Cart.js
@@ -1,35 +1,77 @@
-import { Model } from 'js-abstract-model'
-import { CartItemList } from './CartItem'
+import { Collection, Model } from 'js-abstract-model'
+import { Product } from './Product'
+import Price from './Price'
 
-class Cart extends Model {
+class CartItem extends Model {
   constructor (data) {
     super(data, [
+      { key: 'id' },
       {
-        key: 'items',
-        relatedModel: CartItemList
+        key: 'qty',
+        default: 0
+      },
+      {
+        key: 'product',
+        relatedModel: Product
       }
     ])
   }
 
-  add (item) {
-    this.items.addItem(item)
+  totalPrice () {
+    return new Price({
+      base: this.product.price.base * this.qty,
+      discount: this.product.price.discount * this.qty,
+      final: this.product.price.final * this.qty
+    })
   }
 
-  toman (key, suffix) {
-    if (this[key]) {
-      let string = this[key].toLocaleString('fa')
-      if (typeof suffix === 'undefined' || suffix) {
-        string += ' تومان '
-      }
-
-      return string
-    }
-    return null
-  }
-
-  discountInPercent () {
-    return Math.round(this.discount * 100 / this.base)
+  unitPrice () {
+    return this.product.price
   }
 }
 
-export default Cart
+class Cart extends Collection {
+  model () {
+    return CartItem
+  }
+
+  addItem (product) {
+    const availableItem = this.list.find(function (i) {
+      return (i.product.id === product.id)
+    })
+
+    if (availableItem) {
+      availableItem.qty++
+    } else {
+      super.addItem({
+        product,
+        count: 1
+      })
+    }
+  }
+
+  remove (product) {
+    const availableItem = this.list.find(i => (i.product.id === product.id))
+
+    if (availableItem.qty > 1) {
+      availableItem.qty--
+    } else if (availableItem.qty === 1) {
+      const availableItemIndex = this.list.findIndex(i => (i.product.id === product.id))
+      this.list.splice(availableItemIndex, 1)
+    }
+  }
+
+  totalPrice () {
+    return new Price({
+      base: this.list.reduce((accumulator, item) => { return accumulator + (item.product.price.base * item.qty) }, 0),
+      discount: this.list.reduce((accumulator, item) => { return accumulator + (item.product.price.discount * item.qty) }, 0),
+      final: this.list.reduce((accumulator, item) => { return accumulator + (item.product.price.final * item.qty) }, 0)
+    })
+  }
+
+  qty () {
+    return this.list.reduce((accumulator, item) => { return accumulator + item.qty }, 0)
+  }
+}
+
+export { CartItem, Cart }
