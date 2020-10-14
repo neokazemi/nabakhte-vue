b8b48c5fd8038c146e2017587b2f2eaeda3cc0a4
kerasus <aliesmaeeli20@gmail.com>
2020-10-14 11:49:04 +0330

    asyncData fetch ProductList

--- plugins/mixinProduct.js
+++ plugins/mixinProduct.js
@@ -1,6 +1,17 @@
 import { ProductList } from '~/models/Product'
 
 const mixinProduct = {
+  asyncData (context) {
+    // context.store.commit('updateProducts', new ProductList())
+    return (new ProductList()).fetch()
+      .then((response) => {
+        console.log('response', response)
+        context.store.commit('updateProducts', new ProductList(response.data[0].data.data))
+      }).catch(function (e) {
+        console.log('e', e)
+        context.store.commit('updateProducts', new ProductList())
+      })
+  },
   computed: {
     products () {
       if (this.$store.getters.products instanceof ProductList) {
