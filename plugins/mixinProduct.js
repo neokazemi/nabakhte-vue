69127785dbf3953f80d758dbc58114e90676715e
kerasus <aliesmaeeli20@gmail.com>
2020-10-14 11:50:20 +0330

    remove console.log

--- plugins/mixinProduct.js
+++ plugins/mixinProduct.js
@@ -2,13 +2,10 @@ import { ProductList } from '~/models/Product'
 
 const mixinProduct = {
   asyncData (context) {
-    // context.store.commit('updateProducts', new ProductList())
     return (new ProductList()).fetch()
       .then((response) => {
-        console.log('response', response)
         context.store.commit('updateProducts', new ProductList(response.data[0].data.data))
       }).catch(function (e) {
-        console.log('e', e)
         context.store.commit('updateProducts', new ProductList())
       })
   },
