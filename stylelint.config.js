17dbd602dbe23fc38cde34cfe3dbcd37ee03bb85
kerasus <aliesmaeeli20@gmail.com>
2020-10-18 16:13:11 +0330

    stylelint-config-sass-guidelines

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard'
+    'stylelint-config-standard',
+    'stylelint-config-sass-guidelines'
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  rules: {
+    'selector-class-pattern': null
+  }
 }
