272674f699657bbdb426fe4ddc9fc15e5be5a57e
kerasus <aliesmaeeli20@gmail.com>
2020-10-18 16:52:55 +0330

    selector-max-compound-selectors

--- stylelint.config.js
+++ stylelint.config.js
@@ -6,6 +6,8 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    'selector-class-pattern': null
+    'selector-class-pattern': null,
+    'selector-max-universal': 5,
+    'selector-max-compound-selectors': 5
   }
 }
