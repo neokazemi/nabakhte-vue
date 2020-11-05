ff20d8b8e3b699b25276947a971d7eb81ff7724b
neokazemi <neo.kazemi168@gmail.com>
2020-11-05 10:09:33 +0330

    admin menu

--- components/admin/menus/NestedDraggableNewLink.vue
+++ components/admin/menus/NestedDraggableNewLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <draggable class="dragArea" tag="ul" :list="tasks" :group="{ name: 'g1', pull: 'clone', put: false }">
+  <draggable v-if="children.length" class="dragArea" tag="ul" :list="children" :group="{ name: 'g1', pull: 'clone', put: false }">
     <!--    <v-card-->
     <!--      v-for="el in tasks"-->
     <!--      :key="el.name"-->
@@ -12,24 +12,61 @@
     <!--      </v-card-title>-->
     <!--      <nested-draggable :tasks="el.tasks" />-->
     <!--    </v-card>-->
-    <v-card v-for="(item, index) in tasks" :key="index" outlined elevation="4" class="ma-3 menu-button">
+    <v-card v-for="(item, index) in children" :key="index" outlined elevation="4" class="ma-3 menu-button">
       <v-card-title :style="{ display: 'flex', 'justify-content': 'space-between', padding: '6px' }">
         <div class="menu-name-on-new-link">
           <v-text-field
-            v-model="item.name"
+            v-model="item.data.title"
             label="نام"
             outlined
             dense
           />
           <v-text-field
-            v-model="item.link"
+            v-model="item.data.link"
             label="لینک"
             outlined
             dense
           />
         </div>
         <div class="buttons">
-          <v-btn icon @click="addItem(tasks[0])">
+          <v-btn icon @click="addItem(children[0])">
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </div>
+      </v-card-title>
+    </v-card>
+  </draggable>
+  <draggable v-else class="dragArea" tag="ul" :list="children" :group="{ name: 'g1', pull: 'clone', put: false }">
+    <!--    <v-card-->
+    <!--      v-for="el in tasks"-->
+    <!--      :key="el.name"-->
+    <!--      outlined-->
+    <!--      elevation="4"-->
+    <!--      class="ma-3"-->
+    <!--    >-->
+    <!--      <v-card-title>-->
+    <!--        {{ el.name }}-->
+    <!--      </v-card-title>-->
+    <!--      <nested-draggable :tasks="el.tasks" />-->
+    <!--    </v-card>-->
+    <v-card v-for="(item, index) in children" :key="index" outlined elevation="4" class="ma-3 menu-button">
+      <v-card-title :style="{ display: 'flex', 'justify-content': 'space-between', padding: '6px' }">
+        <div class="menu-name-on-new-link">
+          <v-text-field
+            v-model="item.data.title"
+            label="نام"
+            outlined
+            dense
+          />
+          <v-text-field
+            v-model="item.data.link"
+            label="لینک"
+            outlined
+            dense
+          />
+        </div>
+        <div class="buttons">
+          <v-btn icon @click="addItem(children[0])">
             <v-icon>mdi-plus</v-icon>
           </v-btn>
         </div>
@@ -47,9 +84,8 @@ export default {
     draggable
   },
   props: {
-    tasks: {
-      required: true,
-      type: Array
+    children: {
+      required: true
     }
   },
   data () {
