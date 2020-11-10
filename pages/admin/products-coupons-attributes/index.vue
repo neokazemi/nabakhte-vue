7581336cc99742008cce192db7faf0416c478ae5
neokazemi <neo.kazemi168@gmail.com>
2020-11-10 15:55:16 +0330

    mixin refactor

--- pages/admin/products-coupons-attributes/index.vue
+++ pages/admin/products-coupons-attributes/index.vue
@@ -40,7 +40,7 @@
 </template>
 
 <script>
-import mixinProduct from '~/plugins/mixinProduct'
+import mixinProduct from '~/plugins/mixin/product'
 
 import Menu from '~/components/admin/menu'
 import ProductsTable from '~/components/admin/productsPanel/tables/productsTable'
