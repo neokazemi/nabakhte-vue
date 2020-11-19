0b896ba5e41fd43c7e69c648a512ebf34dad54db
neokazemi <neo.kazemi168@gmail.com>
2020-11-19 12:58:14 +0330

    block and banner model

---
+++ plugins/mixin/block.js
@@ -0,0 +1,19 @@
+import { BlockList } from '../../models/Block'
+
+const mixinBlock = {
+  asyncData (context) {
+    return (new BlockList()).fetch()
+      .then((response) => {
+        context.store.commit('block/updateBlocks', response.data.data)
+      }).catch(function (e) {
+        context.store.commit('block/updateBlocks', null)
+      })
+  },
+  computed: {
+    blocks () {
+      return this.$store.getters['block/blocks']
+    }
+  }
+}
+
+export default mixinBlock
