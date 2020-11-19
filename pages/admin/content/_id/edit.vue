beb27e46e16f1ef770c9615591da4e8e46d0790b
Heliamh <helia.mhmdi@gmail.com>
2020-11-19 14:49:24 +0330

    content list & edit

---
+++ pages/admin/content/_id/edit.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="d-flex justify-end">
+      <v-btn
+
+        color="#b2dfdb"
+        elevation="3"
+        class="ml-5 mt-5 mb-5"
+        @click="$router.go(-1)"
+      >
+        بازگشت >>
+      </v-btn>
+    </div>
+    <ContentInfo /><div class="d-flex justify-end">
+      <v-btn
+        color="#b2dfdb"
+        elevation="3"
+        class="ml-5 mt-5 mb-5"
+      >
+        اعمال تغییرات
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContentInfo from '../../../../components/admin/InformationCorrections/contentInfo'
+export default {
+  name: 'Edit',
+  components: { ContentInfo }
+}
+</script>
+
+<style scoped>
+  .main {
+    background-color: #8e8e8e;
+
+  }
+
+</style>
