c0ac8a9bff296dd751012f6c0c53ed6c0ebc3d24
kerasus <aliesmaeeli20@gmail.com>
2020-11-28 16:08:22 +0330

    favicon-32x32.png

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon-32x32.png' }
     ]
   },
   /*
